c00ccdcdfd88be31c5de30f7676ae5f4ad7016c8
Shisun <worlddominator007@gmail.com>
2023-01-02 17:24:19 +0700

    Finish #16

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import './base.css'
 
 const app = createApp(App)
 
-app.directive('christmas', (/* todo*/) => {
-  /* todo*/
+app.directive('christmas', (el, val) => {
+  const { arg, value } = val;
+  el.classList.add('christmas-text', arg ?? 'red-green')
+  // bind prop inside `arg`
+  // bind value inside `value`
+  if (value) {
+    el.style.animationDuration = `${value}s`;
+  }
 })
 
 app.mount('#app')
